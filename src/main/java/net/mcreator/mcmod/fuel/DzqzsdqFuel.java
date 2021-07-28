5d6741b6671df22f8c1cd43c741d81af0cb20f73
Dreadiix <icetooxik@hotmail.com>
2021-07-28 12:01:11 +0200

    1

---
+++ src/main/java/net/mcreator/mcmod/fuel/DzqzsdqFuel.java
@@ -0,0 +1,25 @@
+
+package net.mcreator.mcmod.fuel;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.mcmod.McmodModElements;
+
+@McmodModElements.ModElement.Tag
+public class DzqzsdqFuel extends McmodModElements.ModElement {
+	public DzqzsdqFuel(McmodModElements instance) {
+		super(instance, 2);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public void furnaceFuelBurnTimeEvent(FurnaceFuelBurnTimeEvent event) {
+		if (event.getItemStack().getItem() == new ItemStack(Blocks.WARPED_HYPHAE, (int) (1)).getItem())
+			event.setBurnTime(1600);
+	}
+}
